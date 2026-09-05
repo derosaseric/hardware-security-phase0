1ad42c7353e63444d6f5caba01ce799a8c3cfb59
Eric De Rosas <derosaseric@gmail.com>
2026-09-04 22:22:22 -0700

    Add binary output to number bases exercise

--- exercises/number_bases.c
+++ exercises/number_bases.c
@@ -11,8 +11,21 @@ int main(void)
         return 1;
     }
 
+    if (number < 0 || number > 255)
+    {
+        printf("Error, input is not within range!\n");
+        return 1;
+    }
+
     printf("\nDecimal: %d\n", number);
     printf("Hexadecimal: 0x%X\n", number);
+    printf("Binary: ");
+
+    for (int bit = 7; bit >= 0; bit--)
+    {
+        printf("%d", (number >> bit) & 1);
+    }
+    printf("\n");
 
     return 0;
 }
